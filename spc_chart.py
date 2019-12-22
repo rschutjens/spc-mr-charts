17bb9bb4ceb11afeef06bc1135a807aff4e6e6e4
ronbot <therealronbot@outlook.com>
2019-12-22 17:12:13 -0500

    add markdown description of notebook

--- spc_chart.py
+++ spc_chart.py
@@ -13,7 +13,29 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # %% [markdown]
-# # SPC
+# # Guess the Process, Moving Range Charts
+# 
+# One of the easiest ways to start monitor a process is a moving range chart, especially if you have no prior data or knowledge of a process. Besides that, many processes do not allow for grouping up results for samples, so single measurements are the natural way to track it. Moving range charts are also used in many complicated processes as one part of their process control. It is also an introduction to more complicated charting like EWMA, or feedback in engineering control.
+# 
+# Even though there are lots of software packages that can be used to do calculations for control charting, it is very straight forward to implement yourself. See this small repository I made with some code to produce the figures for this post. Feel free to take a look and use for yourself. There is also a bit more information on calculating the limits.
+# 
+# The control limits for the $\bar{X}$ chart are calculated as followed:
+# $$
+# \bar{X} = \frac{\sum_{i=1}^n{x_i}}{n} \\
+# UCL = \bar{X} + 3 \frac{\bar{MR}}{d_2} \\
+# UCL = \bar{X} - 3 \frac{\bar{MR}}{d_2}
+# $$
+# 
+# Here $x_i$ are the measurement values, $d_2=1.128$ is a statistical constant, the center line is given by $\bar{X}$. Below it shows how to calculate $\bar{MR}$ and the control limits for the $MR$ chart. 
+# 
+# $$
+# \bar{MR} = \frac{\sum_{i=2}^n{MR_i}}{n-1} \\
+# MR_i=|x_i-x_{i-1}| \\
+# UCL = D_4 \bar{MR} \\
+# LCL = D_3 \bar{MR}
+# $$
+# 
+# $D_3=0$, and $D_4=3.267$ are statistical constants.
 
 # %%
 df = pd.read_csv('data.csv')
@@ -46,6 +68,8 @@ ax[1].set_title('B')
 
 plt.show()
 
+# %% [markdown]
+# The charts above are two control charts, A dnd B, that follow the same process, with measurements done almost daily. I've set up the control charts next to each other and used the same ranges for the vertical and horizontal axes so it is easier to compare. Both charts behave the same, this is even more clear by looking at the figure below where both are plotted in the same figure.
 
 # %%
 axes = df.plot(x='date', y='A', marker='o', )
@@ -60,7 +84,12 @@ axes.set_ylabel('X')
 
 plt.show()
 
-
-# %%
-
-
+# %% [markdown]
+# At the moment the amount of data is still quite limited, so I won't discuss in or out of control conditions until I get to update the figures with some more data. 
+# 
+# Here is the main question though:
+#  - What process do you think these charts are tracking?
+# 
+# Post your answer in the comment, or ask questions for hints.
+# 
+# Lastly, another way to use the moving range chart is to monitor the measurement system for consistency. In about 2 weeks I should have enough data to show the results for the measurement sysem used to monitor the process above.
